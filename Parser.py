3dc7021b2cfc5c5346fc503b17920fb1bfb353dc
Jairaj <jairaj@HAL-8999.(none)>
2010-05-25 17:02:38 -0400

    Parser now returns subsciber information. Organisational changes.

--- Parser.py
+++ Parser.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-from sys import exit,stderr,argv
-import logging
+from sys import exit,argv
+from logging import error
 from urllib2 import urlopen
 from HTMLParser import HTMLParser
 
@@ -9,28 +9,35 @@ class RedditParser(HTMLParser):
 	def __init__(self):
 		HTMLParser.__init__(self)
 		self.Record = False
+		self.RecordSub = False
 		self.Depth = 0
+		self.Subscribers = 0
 		self.Links = []
 	
 	def handle_starttag(self,tag,attrs):
 		if tag=='div' and ('class','usertext-body') in attrs: self.Record = True
+		if tag=='span' and ('class','number') in attrs: self.RecordSub = True
 		if not self.Record: return
 		if tag=='div': self.Depth += 1
 		elif tag=='a':
 			for (a,v) in attrs:
 				if a=='href': self.Links.append(v)
 	
+	def handle_data(self,data):
+		if self.RecordSub: self.Subscribers = data
+	
 	def handle_endtag(self,tag):
 		if self.Record and tag=='div':
 			self.Depth -= 1
 			self.Record = self.Depth!=0
+		else: self.RecordSub = False
 
-def grab_links(subreddit):
+def get_info(subreddit):
 	site = 'http://www.reddit.com/r/%s/' % subreddit.lower()
 	try: html = urlopen(site).read()
 	except Exception,e:
-		logging.error("Unable to get subreddit '%s'" % subreddit)
-		return set()
+		error("Unable to get subreddit '%s'" % subreddit)
+		return
 	parser = RedditParser()
 	parser.feed(html)
 	reddits = set()
@@ -46,16 +53,17 @@ def grab_links(subreddit):
 			if reddit[-1]=='/': reddit = reddit[:-1]
 			if reddit.find('/')!=-1: continue
 			reddits.add(reddit.lower())
-	return reddits
+	return reddits,parser.Subscribers
 
 if __name__=="__main__":
 	if len(argv)<2:
-		logging.error('No subreddit input found.\n' +
-				'Usage: %s subreddit [subreddit2 [...]]')
-		import sys
-		sys.exit(1)
+		error('No subreddit input found.\n' +
+				'Usage: %s subreddit [subreddit2 [...]]' % argv[0])
+		exit(1)
 	for subreddit in argv[1:]:
 		print 'SUBREDDIT: %s' % subreddit
-		for link in grab_links(subreddit):
-			print '\t%s' % link
+		links,subscribers = get_info(subreddit)
+		print '\tSUBSCRIBERS: %s' % subscribers
+		if links:
+			for link in links: print '\t%s' % link
 		print 
