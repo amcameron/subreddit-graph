44502b666a7e899842c1083ada006ccd166b2c68
Jairaj <jairaj@HAL-8999.(none)>
2010-05-25 20:12:43 -0400

    Parser re-implemented harder, better, faster, stronger, and as a class.

--- Parser.py
+++ Parser.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 from sys import exit,argv
 from logging import error
-from urllib2 import urlopen
 from HTMLParser import HTMLParser
+from httplib import HTTPConnection
 
 class RedditParser(HTMLParser):
 	def __init__(self):
 		HTMLParser.__init__(self)
+		self.reset2()
+	
+	def reset2(self):
 		self.Record = False
 		self.RecordSub = False
 		self.RecordLink = False
@@ -14,6 +17,10 @@ class RedditParser(HTMLParser):
 		self.Subscribers = 0
 		self.Links = []
 		self.LinkNames = []
+	
+	def reset3(self):
+		self.reset()
+		self.reset2()
 	
 	def handle_starttag(self,tag,attrs):
 		if tag=='div' and ('class','usertext-body') in attrs: self.Record = True
@@ -38,40 +45,51 @@ class RedditParser(HTMLParser):
 			self.Record = self.Depth!=0
 		else: self.RecordSub = False
 
-def get_info(subreddit):
-	site = 'http://www.reddit.com/r/%s/' % subreddit.lower()
-	try: html = urlopen(site).read()
-	except Exception as e:
-		error("Unable to get subreddit '%s'\n%s" % (subreddit,e))
-		return
-	parser = RedditParser()
-	parser.feed(html)
-	reddits = set()
-	for link,name in zip(parser.Links,parser.LinkNames):
-		link = link.strip()
-		name = name.strip()
-		if link.find(' ')!=-1: continue
-		elif link[:24]=='http://www.reddit.com/r/': reddit = link[24:]
-		elif link[:3]=='/r/': reddit = link[3:]
-		elif name.find(' ')!=-1: continue
-		elif name[:24]=='http://www.reddit.com/r/': reddit = name[24:]
-		elif name[:17]=='www.reddit.com/r/': reddit = name[17:]
-		elif name[:13]=='reddit.com/r/': reddit = name[13:]
-		elif name[:3]=='/r/': reddit = name[3:]
-		elif name[:2]=='r/': reddit = name[2:]
-		else: continue
-		if reddit[-1]=='/': reddit = reddit[:-1]
-		if reddit.find('/')==-1: reddits.add(reddit.lower())
-	return reddits,parser.Subscribers
+class RedditGetter:
+	def __init__(self):
+		self.c = HTTPConnection('www.reddit.com')
+		self.r = RedditParser()
+	
+	def get_info(self,subreddit):
+		try:
+			self.c.request('GET','/r/%s/' % subreddit.lower())
+			html = self.c.getresponse().read()
+		except Exception as e:
+			error("Unable to get subreddit '%s'\n%s" % (subreddit,e))
+			return
+		self.r.reset3()
+		self.r.feed(html)
+		reddits = set()
+		for link,name in zip(self.r.Links,self.r.LinkNames):
+			link = link.strip()
+			name = name.strip()
+			if link.find(' ')!=-1: continue
+			elif link[:24]=='http://www.reddit.com/r/': reddit = link[24:]
+			elif link[:3]=='/r/': reddit = link[3:]
+			elif name.find(' ')!=-1: continue
+			elif name[:24]=='http://www.reddit.com/r/': reddit = name[24:]
+			elif name[:17]=='www.reddit.com/r/': reddit = name[17:]
+			elif name[:13]=='reddit.com/r/': reddit = name[13:]
+			elif name[:3]=='/r/': reddit = name[3:]
+			elif name[:2]=='r/': reddit = name[2:]
+			else: continue
+			if reddit[-1]=='/': reddit = reddit[:-1]
+			if reddit.find('/')==-1: reddits.add(reddit.lower())
+		return reddits,self.r.Subscribers
+	
+	def __del__(self):
+		self.r.close()
+		self.c.close()
 
 if __name__=="__main__":
 	if len(argv)<2:
 		error('No subreddit input found.\n'+
 				'Usage: %s subreddit [subreddit2 [...]]' % argv[0])
 		exit(1)
+	r = RedditGetter()
 	for subreddit in argv[1:]:
 		print 'SUBREDDIT: %s' % subreddit
-		links,subscribers = get_info(subreddit)
+		links,subscribers = r.get_info(subreddit)
 		print '\tSUBSCRIBERS: %s' % subscribers
 		if links:
 			for link in links: print '\t%s' % link
