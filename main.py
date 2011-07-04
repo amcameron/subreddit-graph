bbf47e339aad7e10813e6415cddcff5c67e2c804
Andrew Cameron <andrew@acameron.org>
2011-07-04 14:17:11 -0400

    PEP8-ify main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-from sys import argv,exit
-from logging import error,basicConfig,WARN
+from sys import argv, exit
+from logging import error, basicConfig, WARN
 
 from RedditGrapher import make_graph
 
-if __name__=='__main__':
+if __name__ == '__main__':
 	basicConfig(level=WARN)
-	if len(argv)<2:
+	if len(argv) < 2:
 		error('No subreddit input found')
-		error('Usage: '+argv[0]+' subreddit [subreddit2 [...]]')
+		error('Usage: ' + argv[0] + ' subreddit [subreddit2 [...]]')
 		exit(1)
-	make_graph(argv[1:],'output.png')
+	make_graph(argv[1:], 'output.png')
